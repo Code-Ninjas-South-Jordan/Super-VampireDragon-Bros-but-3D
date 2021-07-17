37b1dbee7c2da24856ed0471102512a5811d173d
CodeNinjasSJ <57017893+CodeNinjasSJ@users.noreply.github.com>
2021-07-16 18:41:18 -0600

    Started

---
+++ Super VampireDragon Bros but 3D/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public float fadeSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FadeOut()
+    {
+        
+    }
+}
