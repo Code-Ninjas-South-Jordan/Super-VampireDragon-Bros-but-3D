cbbf749cf9c31ae414e1f11f0f84a6204ab337ea
CodeNinjasSJ <57017893+CodeNinjasSJ@users.noreply.github.com>
2021-11-02 18:04:49 -0600

    player movement

--- Super VampireDragon Bros but 3D/Assets/Scripts/MyCharacterController.cs
+++ Super VampireDragon Bros but 3D/Assets/Scripts/MyCharacterController.cs
@@ -10,15 +10,28 @@ public class MyCharacterController : MonoBehaviour
 
     public float speed = 6f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; 
@@ -32,9 +45,15 @@ public class MyCharacterController : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
-            velocity.y += gravity * Time.deltaTime;
-
-            controller.Move(velocity * Time.deltaTime);
         }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
