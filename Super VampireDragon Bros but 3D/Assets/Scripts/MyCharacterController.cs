3833dfaaf8299803889a935809e3fd52a9b96654
Code Ninjas 4 <codeninjassouthjordan@gmail.com>
2021-11-30 18:03:25 -0700

    Movement and pipes

--- Super VampireDragon Bros but 3D/Assets/Scripts/MyCharacterController.cs
+++ Super VampireDragon Bros but 3D/Assets/Scripts/MyCharacterController.cs
@@ -4,56 +4,11 @@ using UnityEngine;
 
 public class MyCharacterController : MonoBehaviour
 {
+    public float speed = 3f;
 
-    public CharacterController controller;
-    public Transform cam;
-
-    public float speed = 6f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
-
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
-
-    Vector3 velocity;
-    bool isGrounded;
-
-    // Update is called once per frame
-    void Update()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if(isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-
+    void FixedUpdate(){
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; 
-
-        if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); 
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-
-        }
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-        
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+        this.transform.position += new Vector3(-vertical * speed, 0, horizontal * speed);
     }
 }
